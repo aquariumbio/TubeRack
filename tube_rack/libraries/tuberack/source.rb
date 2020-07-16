b07e663e06f28dbe650650ed8efed7b7b64bb404
Cannon Mallory <malloc3@uw.edu>
2020-07-16 15:54:04 -0700

    Refactored to remove StripWell Class

--- tube_rack/libraries/tuberack/source.rb
+++ tube_rack/libraries/tuberack/source.rb
@@ -1,192 +1,83 @@
 # frozen_string_literal: true
 
-
-# Factory class for instantiating `PlateLayoutGenerator`
-#
-# @author Devin Strickland <strcklnd@uw.edu>
-class TubeRackFactory
-
-  def self.build(item_list:, object_type:, instructions: true)
-    rack = nil
-    if item_list.all? { |coll| coll.collection? }
-      rack = StripWellRack.new(object_type: object_type)
-    elsif item_list.all? { |item| item.is_a? Item }
-      rack = TubeRack.new(object_type: object_type)
-    else
-      raise IncompatibleObjectType, 'Incompatible Object_Type'
-    end
-    item_list.each { |item| rack.add_item(item) }
-    rack
-  end
-
-end
-
 class TubeRack
   TRUE_ITEM = 'true_item'.to_sym
 
-  attr_reader :object_type, :collection, :rows, :columns, :id
+  attr_reader :matrix, :name, :columns, :rows
 
-  def initialize(object_type:, collection: nil)
-    @collection = collection.nil? ? Collection.new_collection(object_type) : collection
-    @object_type = collection.nil? ? object_type.to_s : collection.object_type.name
-    @collection.mark_as_deleted
-    @rows = @collection.dimensions[0]
-    @columns = @collection.dimensions[1]
-    @id = @collection.id
-  end
-
-  def get_non_empty
-    collection.get_non_empty
+  def initialize(rows, columns, name: nil)
+    @columns = columns
+    @rows = rows
+    @matrix = build_matrix
+    @name = name || 'Tube Rack'
   end
 
   def get_empty
-    collection.get_empty
-  end
-
-  def dimensions
-    [rows, columns]
-  end
-
-  def find(val)
-    if val.is_a? Sample
-      return self.find_sample(val)
-    elsif val.is_a? Item
-      return self.find_item(val)
-    else
-      raise 'not valid object to find'
+    empty = []
+    matrix.each_with_index do |row, row_idx|
+      row.each_with_index do |column, col_idx|
+        empty.push([row_idx, col_idx]) if part(row_idx, col_idx).nil?
+      end
     end
+    empty
   end
 
-  def find_sample(sample)
-    raise 'wrong object type' unless sample.is_a Sample
-    locations = []
-    self.get_non_empty.each do |r, c|
-      locations.push([r, c]) if self.part(r, c).sample == sample
+  def get_non_empty
+    non_empty = []
+    matrix.each_with_index do |row, row_idx|
+      row.each_with_index do |column, col_idx|
+        non_empty.push([row_idx, col_idx]) unless part(row_idx, col_idx).nil?
+      end
     end
-    locations
+    non_empty
   end
-
-  def find_item(item)
-    raise 'wrong object type' unless item.is_a item
-    locations = []
-    self.get_non_empty.each do |r, c|
-      locations.push([r, c]) if self.part(r, c) == item
-    end
-    locations
-  end
-
-  def empty?
-    collection.empty?
-  end
-
-  def full?
-    collection.full?
-  end
-
-  def next(r, c, options = {})
-    collection.next(r, c, options = options)
-  end
-
-  def num_samples
-    collection.num_samples
-  end
-
-  def parts
-    self.get_non_empty.map { |r, c| self.part(r, c) }
-  end
-
-  def set(r, c, x)
-    raise 'part must be an item' unless x.is_a? Item
-    self.add_item(x, location: [r, c])
-  end
-
-  # TODO: I do not like this at all.  But the association wouldn't
-  # associate items properly.  Would only return hash of item params
-  # and upload wasn't working at all.
-  def part(r, c)
-    Item.find(@collection.part(r, c).get(TRUE_ITEM))
-  end
-
-  def add_item(item, location: nil)
-    location = @collection.get_empty.first if location.nil?
-    row = location[0]
-    column = location[1]
-    @collection.set(row,
-                    column,
-                    Sample.find_by_name('Generic Tube Rack'))
-    @collection.part(row, column).associate(TRUE_ITEM, item.id)
-    [row, column]
-  end
-end
-
-class StripWellRack < TubeRack
-
-  TRUE_COLLECTION = 'true collection'.to_sym
 
   def add_item(item)
-    unless item.collection?
-      raise IncompatibleObjectType, 'Cannot add individual'\
-                                     ' Items to a Strip Well Rack' 
+    r, c = next_empty_slot
+    set(item, r, c)
+  end
+
+  def add_items(items)
+    items.each do |item|
+      add_item(item)
     end
-
-    add_strip_well(item)
   end
 
-  # TODO: This also feels gross to me but associations won't hold
-  # the actual item
-  def get_strip_well(row, col)
-    Collection.find(@collection.part(row, col || 0).get(TRUE_COLLECTION))
+  def next_empty_slot
+    matrix.each_with_index do |row, row_idx|
+      row.each_with_index do |column, col_idx|
+        return [row_idx, col_idx] if part(row_idx, col_idx).nil?
+      end
+    end
   end
 
-  # returns all rows that a stripwell occupies
-  def find_stripwell(stripwell)
-    wells = []
+  def set(item, row, colum)
+    matrix[row][colum] = item
+  end
+
+  def find(item)
     get_non_empty.each do |r, c|
-      wells.push([r, c]) if get_strip_well(r, c) == stripwell
+      return [r, c] if part(r, c).id == item.id
     end
-    wells
+    nil
+  end
+
+  def find_multiple(items)
+    locations = []
+    items.each do |item|
+      locations.push(find(item))
+    end
+    locations
+  end
+
+  def part(row, column)
+    matrix[row][column]
   end
 
   private
 
-  def add_single_item(item, location:)
-    row = location[0]
-    column = location[1]
-    @collection.set(row,
-                    column,
-                    Sample.find_by_name('Generic Tube Rack'))
-    part = @collection.part(row, column)
-    part.associate(TRUE_ITEM, item.id)
-    part.associate(TRUE_COLLECTION, item.containing_collection.id) if item.is_part
-    [row, column]
-  end
-
-  def add_strip_well(strip_well)
-    check_dimensions(strip_well)
-    items = strip_well.get_non_empty.map { |loc| strip_well.part(loc[0], loc[1]) }
-    r = get_empty_row
-    items.each_with_index { |item, c| add_single_item(item, location: [r, c]) }
-  end
-
-  def get_empty_row
-    @rows.times do |row|
-      skip = false
-      @columns.times do |column| 
-        next if @collection.part(row, column).nil?
-        skip = true
-      end
-      return row unless skip
-    end
-    raise NoEmptyRows, 'No empty rows in Strip Well Rack'
-  end
-
-  def check_dimensions(strip_well)
-    rows = strip_well.dimensions[0]
-    columns = strip_well.dimensions[1]
-    return unless rows > @rows || columns > @columns
-
-    raise IncompatibleDimensions, 'Strip Well dimensions are incompatible'\
-                                 ' with stripwell rack'
+  def build_matrix
+    Array.new(rows) { Array.new(columns) }
   end
 
 end
--- tube_rack/libraries/tuberackhelper/source.rb
+++ tube_rack/libraries/tuberackhelper/source.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-needs 'Collection Management/CollectionTransfer'
 needs 'Collection Management/CollectionDisplay'
 
 module TubeRackHelper
@@ -11,74 +10,48 @@ module TubeRackHelper
     show do
       title 'Get Tube Rack'
       note 'To help stay organized please get a'\
-           " <b>#{tube_rack.object_type}</b>"
-      note "Make sure that the rack has #{tube_rack.rows} rows and #{tube_rack.columns} columns"
-      separator
-      note 'Using a small piece of tape label the <b>'\
-           "#{tube_rack.object_type}</b> with <b>#{tube_rack.id}</b>"
+           " <b>#{tube_rack.name}</b>"
+      note "Make sure that the rack has <b>at least</b> #{tube_rack.rows}"\
+           " rows and #{tube_rack.columns} columns"
     end
   end
 
-  def show_populate_tube_rack(item_list, tube_rack)
+  def show_add_items(item_list, tube_rack)
+    tube_rack.add_items(item_list)
     item_list.each_slice(3).to_a.each do |chunk|
       chunk.map! { |item| tube_rack.find(item) }
       show do
         title 'Place Samples into Tube Rack'
-        note "Place samples tubes in the <b>#{tube_rack.object_type}</b>"\
+        note "Place samples tubes in the <b>#{tube_rack.name}</b>"\
             ' per the table below'
-        table highlight_collection_rc(tube_rack.collection, chunk) { |r, c|
-        tube_rack.part(r, c).id
+        table highlight_tube_rack_rc(tube_rack, chunk) { |r, c|
+          tube_rack.part(r, c).id
         }
       end
     end
   end
 
-  def show_populate_stripwell_rack(stripwell_list, stripwell_rack)
-    rcx_list = []
-    stripwell_list.each do |stripwell|
-      sub_list = stripwell_rack.find_stripwell(stripwell)
-      sub_list.each do |loc|
-        loc.push(stripwell.id.to_s)
-        rcx_list.push(loc)
-      end
-    end
-
-    show do
-      title 'Place Stripwells into Stripwell Rack'
-      note "Carefully place stripwells into <b>#{stripwell_rack.object_type}</b>"\
-           ' per the table below'
-      note 'Make sure that stripwell number 1 is <b>ALWAYS</b> in column 1'
-      table highlight_collection_rcx(stripwell_rack.collection, rcx_list)
-    end
-  end
-
-  def show_add_items(item_list, tube_rack)
-    if tube_rack.is_a? StripWellRack
-      show_populate_stripwell_rack(item_list, tube_rack)
-    elsif tube_rack.is_a? TubeRack
-      show_populate_tube_rack(item_list, tube_rack)
-    else
-      raise 'Unknown Tube Rack Class'
-    end
-  end
-
   # Directions to transfer media to the collection
-  # @param sample_rack [SampleRack]
+  # @param tube_rack [SampleRack]
   # @param media [Item]
   # @param volume [Volume]
   # @param rc_list [Array<[r,c]>] list of all locations that need media
-  def transfer_media_to_rack(sample_rack:, media:, volume:, rc_list:)
-    total_vol = { units: volume[:units], qty: (volume[:qty] * rc_list.length) }
-    fill_reservoir(media, total_vol)
+  def show_transfer_media_to_rack(tube_rack:, media:, volume:, rc_list:)
+    track_provenance(tube_rack: tube_rack,
+                     media: media,
+                     rc_list: rc_list)
 
     rc_list.group_by { |loc| loc.first }.values.each do |rc_row|
-      association_map = []
-      rc_row.each { |r, c| association_map.push({ to_loc: [r, c] }) }
-      track_provenance(sample_rack: sample_rack, media: media, rc_list: rc_list)
-      multichannel_item_to_collection(to_collection: sample_rack.collection,
-                                      source: "media reservoir",
-                                      volume: volume,
-                                      association_map: association_map)
+      show do
+        title 'Multi Channel Pipet'
+        note multichannel_pipet(volume: volume,
+                           source: 'Media Reservoir',
+                           destination: tube_rack.name)
+        note 'Per table below'
+        table highlight_tube_rack_rc(tube_rack, rc_row, check: true) { |r, c|
+          tube_rack.part(r, c).id
+        }
+      end
     end
   end
 
@@ -87,11 +60,12 @@ module TubeRackHelper
   #
   # @param media (item)
   # @param volume [Volume]
-  def fill_reservoir(media, volume)
+  def show_fill_reservoir(media, unit_volume, number_items)
+    total_vol = { units: unit_volume[:units], qty: (unit_volume[:qty] * number_items) }
     show do
       title 'Fill Media Reservoir'
       check 'Get a media reservoir'
-      check pipet(volume: volume,
+      check pipet(volume: total_vol,
                   source: "<b>#{media.id}</b>",
                   destination: '<b>Media Reservoir</b>')
     end
@@ -99,9 +73,9 @@ module TubeRackHelper
 
   # TODO: This doesn't properly track provenence I dont think
   # Not sure this is belongs here either
-  def track_provenance(sample_rack:, media:, rc_list:)
+  def track_provenance(tube_rack:, media:, rc_list:)
     rc_list.each do |r, c|
-      to_obj = sample_rack.part(r, c)
+      to_obj = tube_rack.part(r, c)
       from_obj_map = AssociationMap.new(media)
       to_obj_map = AssociationMap.new(to_obj)
       add_provenance(from: media, from_map: from_obj_map,
@@ -110,4 +84,21 @@ module TubeRackHelper
       to_obj_map.save
     end
   end
+
+
+  def highlight_tube_rack_rc(tube_rack, rc_list, check: false, &_rc_block)
+    rcx_list = rc_list.map { |r, c|
+      block_given? ? [r, c, yield(r, c)] : [r, c, '']
+    }
+    highlight_tube_rack_rcx(tube_rack, rcx_list, check: check)
+  end
+
+  def highlight_tube_rack_rcx(tube_rack, rcx_list, check: true)
+    tbl = create_collection_table(rows: tube_rack.rows,
+                                  columns: tube_rack.columns,
+                                  col_id: 'Tube Rack')
+    highlight_rcx(tbl, rcx_list, check: check)
+    tbl
+  end
+
 end
